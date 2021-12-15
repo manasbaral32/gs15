be91eb5bd8bb5ce956df7c939a00d4f59577ca85
manasbaral32 <baralmanas32@gmail.com>
2021-12-15 22:56:20 +0530

    Add some view code

--- fees/views.py
+++ fees/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here. 
 def fees_django(request):
-    return render(request,'fees/fees1.html'), 
+    return render(request,'fees/fees1.html'),
+
+
+
+#This is for Git testing purpose
